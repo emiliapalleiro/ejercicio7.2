9acebcb61c09c72fed0801ad6d952f5b633d162c
emiliapalleiro <epalleiro@gmail.com>
2023-08-07 19:36:49 -0300

    Version 1

--- js/script.js
+++ js/script.js
@@ -12,10 +12,19 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  */
 function showData(dataArray) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const item of dataArray.students) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
+
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+  fetch(DATA_URL) // Realizar la solicitud
+  .then(response => response.json()) // Convertir la respuesta a JSON
+  .then(data => showData(data)) // Llamar a la función showData con los datos obtenidos
+  .catch(error => console.error("Error al cargar los datos:", error));
+  
+  
+  
